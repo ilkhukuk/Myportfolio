3765a77aa852e00bca5f1a16532f1c2bfb21b371
ilkhukuk <ilkhukuk@gmail.com>
2024-01-14 10:30:26 +0300

    translate

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -35,7 +35,7 @@ const Services = () => {
       </motion.div>
       <motion.div>
         <motion.p>
-          {translate
+          {!translate
             ? "I am a self-motivated individual who is disciplined and organized. Education is of great importance to me, and I am always open to learning. I enjoy keeping up with technological advancements and stay updated in this field. I believe having excellent communication skills is essential to succeed in the professional life. I actively participate as a team player in collaborative work environments. I possess strong stress management skills, and challenges have played a significant role in my personal development. I always do my best to succeed, and I am determined to overcome challenges. When faced with difficulties, I focus on the causes and solutions rather than the problems themselves. Collaboration, support, and self-sacrifice are values that I hold dear. I enjoy interacting with people and go the extra mile to assist others. Furthermore, I am passionate about my profession and dedicated to my work. I highly value nature, people, and especially my loved ones. These values not only motivate me in my personal life but also make my work more meaningful."
             : "Öğrenme tutkusu olan, azimli ve organize biriyim. Teknolojik gelişmeleri takip etmekten ve bu alanda güncel kalmaktan keyif alıyorum. Mükemmel iletişim becerilerinin profesyonel yaşamda başarılı olmak için gerekli olduğuna inanıyorum. İşbirlikçi çalışma ortamlarında aktif olarak ekip oyuncusu olarak katılırım. Güçlü stres yönetimi becerilerine sahibim ve zorluklar kişisel gelişimimde önemli bir rol oynamıştır. Her zaman başarmak için elimden gelenin en iyisini yaparım ve zorlukların üstesinden gelmeye kararlıyım. Zorluklarla karşılaştığımda, problemlerin kendisinden ziyade sebeplere ve çözümlere odaklanırım. İşbirliği, destek ve özveri benim önem verdiğim değerlerdir. İnsanlarla etkileşim kurmaktan ve başkalarına yardım etmek için fazladan çaba göstermekten zevk alıyorum. Ayrıca mesleğime tutkulu ve işime adanmış biriyim. Doğaya, insanlara ve özellikle aileme ve sevdiklerime büyük değer veriyorum. Bu değerler sadece kişisel hayatımda beni motive etmiyor, aynı zamanda işimi daha anlamlı kılıyor."}
         </motion.p>
@@ -49,7 +49,7 @@ const Services = () => {
               Download CV
             </a>
           </motion.button>
-          <button onClick={() => setTranslate(!translate)}>Translate</button>
+          <button onClick={() => setTranslate(!translate)}>{translate ? "English" : "Türkçe"}</button>
         </motion.div>
       </motion.div>
     </motion.div>
